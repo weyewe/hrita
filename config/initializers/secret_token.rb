9fa20e5362ce04995d70de9d9e04e916e29b9ace
weyewe <weyewe@Willy-Yunnals-MacBook.local>
2011-03-17 21:52:27 +0700

    initial rails setting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrita::Application.config.secret_token = '03a1234a4018a00e0a33942fc921967952a6c3e309bf397eef48f8afadfc5056e5516fb2f264a8346122560109d8d4fac2b7124549641279739f0be2604ca004'
